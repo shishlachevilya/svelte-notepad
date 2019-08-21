86051aa15c63f17649986e5cc2e9d298261b4dcc
ilyashishlachev <ilya.shishlachev@techs.com.ua>
2019-08-21 10:48:40 +0300

    added development branch

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
   import NoteItem from "./components/NoteItem.svelte";
   import Popup from "./components/Popup.svelte";
   import shortid from "shortid";
-  import { getLocalNotes } from "./helpres/getLocalNotes.js";
-  import { setLocalNotes } from "./helpres/setLocalNotes.js";
+  import {getLocalNotes} from "./helpres/getLocalNotes.js";
+  import {setLocalNotes} from "./helpres/setLocalNotes.js";
 
   let isOpen = false;
 
@@ -13,28 +13,28 @@
       id: "XWaQXcbk0",
       title: "JavaScript essentials",
       body:
-        "Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc",
+          "Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc",
       priority: "HIGH"
     },
     {
       id: "pkXzyRp1P",
       title: "Refresh HTML and CSS",
       body:
-        "Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.",
+          "Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.",
       priority: "NORMAL"
     },
     {
       id: "QMom9q4Ku",
       title: "Get comfy with Frontend frameworks",
       body:
-        "First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.",
+          "First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.",
       priority: "NORMAL"
     },
     {
       id: "k2k0UrjZG",
       title: "Winter clothes",
       body:
-        "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+          "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
       priority: "LOW"
     }
   ];
@@ -46,10 +46,10 @@
   };
 
   $: filteredNotes = prefix.query
-    ? notes.filter(note =>
-        note.body.toLowerCase().includes(prefix.query.toLowerCase())
+      ? notes.filter(note =>
+          note.body.toLowerCase().includes(prefix.query.toLowerCase())
       )
-    : notes;
+      : notes;
 
   function newNote(event) {
     notes = [
@@ -74,16 +74,18 @@
     prefix.change = false;
     prefix.query = event.detail.value.toLowerCase();
   }
+
   function handleDeleteNote(event) {
-    const { id } = event.detail;
+    const {id} = event.detail;
     // ссылка на notes не должна меняться, поэтому мутируем объект
     notes.splice(notes.indexOf(e => e.id === id), 1);
     setLocalNotes(notes);
   }
+
   function handleSaveLocal(event) {
-    const { id, title, body, priority } = event.detail;
+    const {id, title, body, priority} = event.detail;
     let note = notes.find(e => e.id === id);
-    Object.assign(note, { id, title, body, priority });
+    Object.assign(note, {id, title, body, priority});
     setLocalNotes(notes);
   }
 </script>
@@ -99,23 +101,23 @@
 </style>
 
 <div>
-  <Header on:search={filterNotes} on:modal={openHandle} />
+  <Header on:search={filterNotes} on:modal={openHandle}/>
 
   <main class="container">
     <ul class="note-list">
       {#each filteredNotes as { id, title, body, priority }}
         <NoteItem
-          {id}
-          {title}
-          {body}
-          {priority}
-          on:delete={handleDeleteNote}
-          on:saveLocal={handleSaveLocal} />
+            {id}
+            {title}
+            {body}
+            {priority}
+            on:delete={handleDeleteNote}
+            on:saveLocal={handleSaveLocal}/>
       {/each}
     </ul>
   </main>
 
   {#if isOpen}
-    <Popup on:note={newNote} on:modal={openHandle} />
+    <Popup on:note={newNote} on:modal={openHandle}/>
   {/if}
 </div>
